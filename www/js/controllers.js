18a2db5c98d93570cc92d8954894fe8e8c4b6152
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-12-12 00:59:31 -0800

    handle errors, timeout quicker

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,15 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope, $http) {
+  var httpConfig = { timeout: 1000 }
 
   $scope.things = [{
     label: "Garage Door",
     state: "unknown",
     init: function() {
       var self = this;
-      console.log('inited');
       var fetchState = function() {
-        $http.post('http://garage:4000/digital_read/4')
+        $http.post('http://garage:4000/digital_read/4', {}, httpConfig)
         .success(function(res) {
           if (res.value === 1) {
             self.state = "Open"
@@ -17,18 +17,27 @@ angular.module('starter.controllers', [])
             self.state = "Closed"
           }
         })
+        .error(function() {
+          self.state = "unknown"
+        })
       }
       fetchState();
       setInterval(fetchState, 5000);
     },
     toggle: function() {
-      $http.post('http://garage:4000/digital_write/18/1').success(function() {
+      $http.post('http://garage:4000/digital_write/18/1', {}, httpConfig).success(function() {
         console.log('wrote 1');
         setTimeout(function() {
-          $http.post('http://garage:4000/digital_write/18/0').success(function() {
+          $http.post('http://garage:4000/digital_write/18/0', {}, httpConfig).success(function() {
             console.log('wrote 0');
           })
+          .error(function() {
+            alert('failed to write 0! stuck in on position!')
+          })
         }, 800)
+      })
+      .error(function() {
+        alert('failed to write 1!')
       })
     }
   }];
