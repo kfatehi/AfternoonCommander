c4db0720170c70202fff70c5ebdad1c19bd69b40
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-03-05 19:57:42 -0800

    camera, events, transcode, etc

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,46 +1,107 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $http) {
+.controller('DashCtrl', function($scope, $http, $ionicPopup, $ionicPlatform, $ionicModal, $sce) {
   var httpConfig = { timeout: 1000 }
 
-  var garage = '192.168.1.11';
+  $scope.doRefresh = function() {
+    $scope.things.forEach(function(thing) {
+      if (thing.init && !thing.disabled) thing.init();
+      setTimeout(function() {
+        $scope.$broadcast('scroll.refreshComplete');
+      }, 2000);
+    });
+  }
 
   $scope.things = [{
-    label: "Garage Door",
-    state: "unknown",
+    label: "Camera: Entrance",
+    state: "Last Event: unknown",
     init: function() {
       var self = this;
-      var fetchState = function() {
-        $http.post('http://'+garage+':4000/digital_read/4', {}, httpConfig)
+      var src = self.imgSrc;
+      $scope.events = [];
+      self.imgSrc = ''; // for refresh purposes
+      self.imgSrc = "http://camera:8080/";
+      $http.get("http://lab:8000/movies").success(function(events) {
+        if (events.length) {
+          events.forEach(function(m) {
+            $scope.events.push({
+              time: new Date(m.time).toLocaleString(),
+              url: "http://lab:8000"+m.path
+            })
+          })
+          self.state = "Last Event: "+$scope.events[0].time
+        } else {
+          self.state = "Last Event: unknown"
+        }
+        $scope.setMovieURL = function(url) {
+          $scope.movieURL = $sce.trustAsResourceUrl(url);
+          $ionicModal.fromTemplateUrl('video-modal.html',{
+            scope: $scope
+          }).then(function(modal) {
+            $scope.closeVideoModal = function() { modal.hide() };
+            modal.show();
+          });
+        }
+      })
+    },
+    buttons: [{
+      text: "Events",
+      action: function() {
+        $ionicModal.fromTemplateUrl('events-modal.html',{
+          scope: $scope
+        }).then(function(modal) {
+          $scope.closeEventsModal = function() {
+            modal.hide();
+          };
+          modal.show();
+        });
+      }
+    }]
+  },{
+    label: "Garage Door",
+    state: "Status: unknown",
+    init: function() {
+      var self = this;
+      function fetchState() {
+        $http.post('http://garage:4000/digital_read/4', {}, httpConfig)
         .success(function(res) {
           if (res.value === 1) {
-            self.state = "Open"
+            self.state = "Status: Open"
           } else if (res.value === 0) {
-            self.state = "Closed"
+            self.state = "Status: Closed"
           }
         })
         .error(function() {
-          self.state = "unknown"
+          self.state = "Status: unknown"
         })
       }
       fetchState();
-      setInterval(fetchState, 5000);
+      if (self.interval) clearInterval(self.interval); // for refresh purposes
+      self.interval = setInterval(fetchState, 5000);
     },
-    toggle: function() {
-      $http.post('http://'+garage+':4000/digital_write/18/1', {}, httpConfig).success(function() {
-        console.log('wrote 1');
-        setTimeout(function() {
-          $http.post('http://'+garage+':4000/digital_write/18/0', {}, httpConfig).success(function() {
-            console.log('wrote 0');
-          })
-          .error(function() {
-            alert('failed to write 0! stuck in on position!')
-          })
-        }, 800)
-      })
-      .error(function() {
-        alert('failed to write 1!')
-      })
-    }
+    buttons: [{
+      text: "Activate",
+      action: function() {
+        $ionicPopup.confirm({
+          title: "Safety Check",
+          template: "Are you sure it is safe to activate the garage door?",
+          okText: "Yes. Do it."
+        }).then(function(yes) {
+          if (yes) {
+            $http.post('http://garage:4000/digital_write/18/1', {}, httpConfig).success(function() {
+              console.log('wrote 1');
+              setTimeout(function() {
+                $http.post('http://garage:4000/digital_write/18/0', {}, httpConfig).success(function() {
+                  console.log('wrote 0');
+                })
+                .error(function() {
+                  alert('failed to write 0! stuck in on position!')
+                })
+              }, 800)
+            }).error(function() { alert('failed to write 1!') })
+          }
+        })
+      }
+    }]
   }];
 })
